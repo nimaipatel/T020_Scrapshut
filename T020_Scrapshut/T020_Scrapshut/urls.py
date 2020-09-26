39ceaea06738224c3840cb4aa220d793a3572326
Nimai Patel <nimai.m.patel@gmail.com>
2020-09-26 19:14:34 +0530

    changed routes

--- T020_Scrapshut/T020_Scrapshut/urls.py
+++ T020_Scrapshut/T020_Scrapshut/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("core.urls")),
+    path('ngos/', include("core.urls")),
     path('', include('Donor.urls'))
 ]
