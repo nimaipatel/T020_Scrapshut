dff930046275fee516cf0ee4d8d4057dd7580889
Nimai Patel <nimai.m.patel@gmail.com>
2020-09-26 14:48:26 +0530

    started working on NgoAdmin model

--- T020_Scrapshut/core/models.py
+++ T020_Scrapshut/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class NgoAdmin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+
+    def __srt__(self):
+        return self.user.name
